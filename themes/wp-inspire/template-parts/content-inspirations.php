2bd60d0c170fe02466ea06e94263b6e94f17b2ec
Oliver <oliver.harrison@webdevstudios.com>
2020-03-27 16:05:29 -0400

    refining the layout of the cards

--- themes/wp-inspire/template-parts/content-inspirations.php
+++ themes/wp-inspire/template-parts/content-inspirations.php
@@ -15,40 +15,39 @@ $inspirations = new WP_Query(
 );
 ?>
 
+<div class="inspirations-bg"></div>
 <div class="container">
 
 <?php if ( $inspirations->have_posts() ) : ?>
 
 	<section class="inspirations-block row">
-	<?php while ( $inspirations->have_posts() ) : $inspirations->the_post(); ?>
+		<?php while ( $inspirations->have_posts() ) : $inspirations->the_post(); ?>
 
-		<?php
-			$insp_link = get_field( 'link' );
-		?>
-		<article id="post-<?php the_ID(); ?>" <?php post_class( 'inspiration col-sm-4' ); ?>>
+			<?php $insp_link = get_field( 'link' ); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class( 'inspiration col-md-6 col-lg-4' ); ?>><!--  -->
 
-			<div class="card">
-				<div class="card-shadow"></div>
-				<main class="post-body">
+				<div class="card">
+					<div class="card-shadow"></div>
+					<main class="post-body">
 
-					<?php wp_inspire_post_thumbnail(); ?>
-					<?php wp_inspire_entry_body(); ?>
+						<?php wp_inspire_post_thumbnail(); ?>
+						<?php wp_inspire_entry_body(); ?>
 
-				</main><!-- .entry-body -->
+					</main><!-- .entry-body -->
 
-				<footer class="post-footer">
-					<?php the_title( '<h2 class="inspiration-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
-					<?php if ( $insp_link ) : ?>
-						<a href="<?php echo esc_url( $insp_link['url'] ); ?>" class="link-out" target="_blank"><span class="icon icon-link"><?php echo esc_html( preg_replace( '#^https?://#', '', rtrim( $insp_link['url'], '/' ) ) ); ?></span></a>
-					<?php endif; ?>
+					<footer class="post-footer">
+						<?php the_title( '<h2 class="inspiration-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+						<?php if ( $insp_link ) : ?>
+							<a href="<?php echo esc_url( $insp_link['url'] ); ?>" class="link-out" target="_blank"><span class="icon icon-link"><?php echo esc_html( preg_replace( '#^https?://#', '', rtrim( $insp_link['url'], '/' ) ) ); ?></span></a>
+						<?php endif; ?>
 
-				</footer><!-- .entry-footer -->
+					</footer><!-- .entry-footer -->
 
-			</div><!-- .card -->
+				</div><!-- .card -->
 
-		</article><!-- #post-<?php the_ID(); ?> .inspiration.col-sm-4 -->
+			</article><!-- #post-<?php the_ID(); ?> .inspiration.col-sm-4 -->
 
-	<?php endwhile; ?>
+		<?php endwhile; ?>
 
 	<?php
 	if (  $inspirations->max_num_pages > 1 ) :
